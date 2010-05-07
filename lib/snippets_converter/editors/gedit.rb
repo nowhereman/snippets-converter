83645c342a8e131459d4cb7639a965e3b6685cdf
Nowhere Man <nowhereman@open_desktop>
2010-05-07 18:16:44 +0200

    ReIndenting Gedit editor.

--- lib/snippets_converter/editors/gedit.rb
+++ lib/snippets_converter/editors/gedit.rb
@@ -5,18 +5,18 @@ module SnippetsConverter
       def editor_conversion(trigger, description, code)
         # Need to remove dot in 'trigger' because Gedit doesn't seem to like it on the tag
         return <<-CODE
-    <snippet id="#{trigger.gsub(/\./,'')}">
-      <tag>#{trigger.gsub(/\./,'')}</tag>
-      <description><![CDATA[#{description}]]></description>
-      <text><![CDATA[#{code}]]></text>
-    </snippet>
+  <snippet id="#{trigger.gsub(/\./,'')}">
+    <tag>#{trigger.gsub(/\./,'')}</tag>
+    <description>#{description}</description>
+    <text><![CDATA[#{code}]]></text>
+  </snippet>
         CODE
       end
 
       def editor_header(language = 'ruby')
         return <<-CODE
-  <?xml version='1.0' encoding='utf-8'?>
-  <snippets language=\"#{language}\">
+<?xml version='1.0' encoding='utf-8'?>
+<snippets language=\"#{language}\">
         CODE
       end
 
